58ff9d88d3382370ea2f843630af720688118d20
PauloGLemos <paulogabrieldeangelislemos@gmail.com>
2021-05-11 20:38:05 -0300

    Inclusão de arquivos das aulas 6 e 7

---
+++ Aula07.py
@@ -0,0 +1,42 @@
+'''def imprime_oi():
+    print("Oi")
+
+imprime_oi()
+
+global a
+a = 10
+print(a)
+
+def soma(x, y):
+    a = 20
+    return x + y + a
+
+x = soma(10,5)
+#b = soma(a,5)
+print(x)
+
+def soma(x, y):
+    global total
+    total = x + y
+
+    total = 10
+
+    soma(3,5)
+    total = 10
+    print("Valor", total)'''
+
+def aumento_salarial(salario, percentual):
+        novo_salario = salario*percentual/100 + salario
+        return
+
+salario_fulano = aumento_salarial(5000,1)
+print(salario_fulano)
+
+import math
+
+a = math.pi
+print(a)
+
+
+
+
