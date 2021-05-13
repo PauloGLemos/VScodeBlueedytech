e1e2564a1b83210bf005750901d4aa6e8c25e044
PauloGLemos <paulogabrieldeangelislemos@gmail.com>
2021-05-12 23:17:10 -0300

    exercicios aula 6

---
+++ Exercicios aula 6.py
@@ -0,0 +1,20 @@
+#1. Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+def soma():
+        var1 = int(input("Digite num1:" ))
+        var2 = int(input("Digite num2:" ))
+        var3 = int(input("Digite num3:" ))
+
+        somas = (var1 + var2 + var3)   
+        print(f"A soma dos valores é:", {somas})
+soma()
+
+def empresaXYZ():
+    horas = int(input("Digite horas trabalhadas: "))
+
+    if horas >= 40:
+        print("voce recebera um extra de 1.5 por horas trabalhadas",  (horas * 1.5) * 100)
+    else:
+        print("seu salario e igual a", horas * 100)
+
+empresaXYZ()
